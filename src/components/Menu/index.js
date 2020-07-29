ca54c251913d374f33c957964e62a003f46269a1
tutss <artur_santos@usp.br>
2020-07-29 16:55:10 -0300

    aula 2 - 58min

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 // import ButtonLink from './components/ButtonLink';
-import Button from '../Button'
+import Button from '../Button';
+
+import { Link } from 'react-router-dom';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="tutsvids logo"/>
-            </a>
+            </Link>
 
-            <Button as='a' className="ButtonLink" href='/'>
+            <Button as={Link} className="ButtonLink" to='/cadastro/video'>
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio do jogo
+const P404 = () => (<div>Página 404</div>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={Home} exact/>
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route path='/cadastro/categoria' component={CadastroCategoria} />
+      <Route component={P404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
