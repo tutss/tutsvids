ebe340c9d48f29ec20c6e9a5a739feea188482b9
tutss <artur_santos@usp.br>
2020-08-04 12:06:29 -0300

    Aula 4 - 1h10

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -24,6 +24,16 @@ function CadastroCategoria() {
     // const { getAttribute, value } = eventInfo.target;
     setValue(eventInfo.target.getAttribute('name'), eventInfo.target.value);
   }
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL).then(async (response) => {
+      const r = await response.json();
+      setCategorias([
+        ...r,
+      ]);
+    });
+  }, []);
 
   return (
     <PageDefault>
@@ -71,6 +81,12 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
 
       <ul>
         {
